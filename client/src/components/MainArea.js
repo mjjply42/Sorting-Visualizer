1e7047c9c1c485f09dd22c8addff1e30ea5872eb
matthew-infiswift <mjjply2@gmail.com>
2020-01-10 18:37:31 -0800

    testing size changes

--- client/src/components/MainArea.js
+++ client/src/components/MainArea.js
@@ -4,17 +4,28 @@ export const MainArea = () => {
 
     const [height, setHeight] = useState("20em")
 
-    function handler_1800 () {
-      setHeight("150em")
-    }
+    /*function handler_2200 () {
+        setHeight("190em")
+      }
     function handler_2000 () {
-      setHeight("137em")
+        setHeight("140em")
+      }
+    function handler_1800 () {
+      setHeight("100em")
     }
-  
-    //window.matchMedia("(min-height: 1800px)").addListener(handler_1800)
-    window.matchMedia("(min-height: 2000px)").addListener(handler_2000)
+    function handler_1000 () {
+        setHeight("80em")
+      }
+      function handler_900 () {
+        setHeight("40em")
+      }
+    window.matchMedia("(max-height: 900px)").addListener(handler_900)
+    window.matchMedia("(max-height: 1000px)").addListener(handler_1000)
+    window.matchMedia("(max-height: 1800px)").addListener(handler_1800)
+    window.matchMedia("(max-height: 2000px)").addListener(handler_2000)
+    window.matchMedia("(max-height: 2200px)").addListener(handler_2200)*/
     return (
-        <div style={{marginLeft: 90, marginTop: 80, backgroundColor: "yellow", height: height, width: "88%"}}>
+        <div style={{marginLeft: 90, marginTop: 80, backgroundColor: "yellow", height: 600, width: "92%"}}>
             <h>Hello</h>
         </div>
     )
